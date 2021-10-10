c5c807855ccf2cf2187e39ce29f912db5dea3a54
Nadir Hussain <nadirarain111@gmail.com>
2021-10-10 20:37:36 +0500

    first version

---
+++ app/src/main/java/com/example/tipcalculator/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.example.tipcalculator
+
+import android.annotation.SuppressLint
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.KeyEvent
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import com.example.tipcalculator.databinding.ActivityMainBinding
+import java.text.NumberFormat
+import kotlin.math.ceil
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+        binding.calculateButton.setOnClickListener {
+
+            calculateTip()
+
+        }
+
+        binding.costOfServiceEditText.setOnKeyListener{view, keyCode, _ -> handleKeyEvent(view, keyCode)}
+
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KeyEvent.KEYCODE_ENTER) {
+            // Hide the keyboard
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
+    }
+
+    @SuppressLint("SetTextI18n")
+    private fun calculateTip(){
+
+        val stringInTextField=binding.costOfServiceEditText.text.toString()
+
+        val cost=stringInTextField.toDoubleOrNull()
+
+        val tipPercentage = when(binding.tipOption.checkedRadioButtonId){
+            R.id.option_20_percent->0.20
+            R.id.option_18_percent->0.18
+            else->0.15
+        }
+        if (cost==null){
+            binding.tipResult.text="Please Enter Amount."
+            return
+        }
+
+        var tip=tipPercentage*cost
+
+        val roundUp=binding.roundUpSwitch.isChecked
+
+        if (roundUp){
+
+            tip= ceil(tip)
+
+        }
+
+        val formattedTip=NumberFormat.getCurrencyInstance().format(tip)
+
+        binding.tipResult.text=getString(R.string.tip_amount,formattedTip)
+
+
+    }
+
+
+
+}
